b335dfe623bf0ca478c0170361a5843205f083e8
Prateek Joshi <padi@MacBook-Pro-2.local>
2023-05-29 15:00:29 -0400

    initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <>
+    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full"></div>
+    </div>
+  </>
+}
+
+export default page

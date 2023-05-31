8441c0e4ae73a41b6a242f30ea6ac19b0e2f10c2
Prateek Joshi <padi@MacBook-Pro-2.local>
2023-05-31 17:56:39 -0400

    login page but g OAuth not working

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,18 @@
 import { FC } from 'react'
 import Button from '@/components/ui/Button'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
 
 interface pageProps {
   
 }
 
-const page: FC<pageProps> = ({}) => {
+const page = async ({}) => {
+
+  const session = await getServerSession(authOptions);
+
+  return <pre>{JSON.stringify(session)}</pre>
+
   return <Button>hello</Button>
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ import { db } from "@/lib/db";
 
 export default async function Home() {
 
-  return <Button variant='ghost'>Hello</Button>
+  return <Button>Hello</Button>
 }
